4e519a03d71dde8be9a0c6ab0a96539e03e6f1d6
req12345 <req12345@mail.ru>
2021-08-13 13:15:14 +0300

    Task5/controllers testing (#2)
    * Install Slim
    
    * Set generators config
    
    * Questions controller with rspec for index
    
    Set rails helpers
    
    * Question controller with tests
    
    * Answers controller with tests
    
    * Answer controller without callback load_answer/question
    
    * Bug fix babel config for private-methods
    
    * Configure .rubocop.yml

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,31 @@
+class AnswersController < ApplicationController
+  def show; end
+
+  def new; end
+
+  def create
+    @answer = question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:title, :body, :question_id)
+  end
+
+  def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  helper_method :question, :answer
+end
