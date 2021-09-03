c6b5b4d4909dfd41c8375b61cad81a73c4e070e4
Ivan Bakhmetov <req12345@mail.ru>
2021-09-04 00:38:27 +0300

    Refactoring & small fixes

--- spec/features/answers/destroy_spec.rb
+++ spec/features/answers/destroy_spec.rb
@@ -8,48 +8,52 @@ feature 'User can delete answer', %q{
   given(:user) { create(:user) }
   given(:not_author) { create(:user) }
   given(:question) { create(:question, author: user) }
-  given(:answer) { create(:answer, question: question, author: user) }
+  given(:answer) {create(:answer, question: question, author: user) }
   given(:answer_with_file) { create(:answer, :with_file, question: question, author: user) }
 
-  scenario 'Author can delete attachments', js: true do
-    sign_in(answer_with_file.author)
-    visit question_path(answer_with_file.question)
+  describe 'Author' do
+    before do
+      sign_in(answer_with_file.author)
+      visit question_path(answer_with_file.question)
+    end
 
-    within '.answers' do
-      expect(page).to have_link file_name(answer_with_file)
-      click_on 'Delete file'
+    scenario 'can delete attachments', js: true do
+      within '.answers' do
+        expect(page).to have_link file_name(answer_with_file)
+        click_on 'Delete file'
 
-      expect(page).to_not have_link file_name(answer_with_file)
-      expect(page).to_not have_link 'Delete file'
+        expect(page).to_not have_link file_name(answer_with_file)
+        expect(page).to_not have_link 'Delete file'
+      end
+    end
+
+    scenario 'delete his question', js: true do
+      expect(page).to have_content answer_with_file.body
+      click_on 'Delete answer'
+      expect(page).to have_content 'Your answer deleted'
+      expect(page).to_not have_content(answer_with_file.body)
     end
   end
 
-  scenario 'Not author can not delete attachments', js: true do
-    sign_in(not_author)
-    visit question_path(answer_with_file.question)
-    within '.answers' do
-      expect(page).to_not have_link 'Delete file'
+  describe 'Not author' do
+    before do
+      sign_in(not_author)
+      visit question_path(answer_with_file.question)
     end
-  end
 
-  scenario 'Author delete his question', js: true do
-    sign_in(answer.author)
-    visit question_path(answer.question)
-    expect(page).to have_content answer.body
-    click_on 'Delete answer'
-    expect(page).to have_content 'Your answer deleted'
-    expect(page).to_not have_content(answer.body)
-  end
+    scenario 'Not author can not delete attachments', js: true do
+      within '.answers' do
+        expect(page).to_not have_link 'Delete file'
+      end
+    end
 
-  scenario 'Not author can not delete question' do
-    sign_in(not_author)
-    visit question_path(question)
-
-    expect(page).to_not have_link 'Delete answer'
+    scenario 'Not author can not delete question', js: true do
+      expect(page).to_not have_link 'Delete answer'
+    end
   end
 
   scenario 'Unauthenticated user can not delete question' do
-    visit question_path(question)
+    visit question_path(answer.question)
 
     expect(page).to_not have_link 'Delete answer'
   end
--- spec/features/questions/destroy_spec.rb
+++ spec/features/questions/destroy_spec.rb
@@ -8,53 +8,57 @@ feature 'User can delete question', %q{
 
   given(:user) { create(:user) }
   given(:not_author) { create(:user) }
-  given(:question) { create(:question, author: user) }
+  given(:question) { create(:question) }
   given(:question_with_file) { create(:question, :with_file, author: user) }
 
-  scenario 'Author can delete attachments', js: true do
-    sign_in(question_with_file.author)
-    visit question_path(question_with_file)
+  describe 'Author' do
+    before do
+      sign_in(question_with_file.author)
+      visit question_path(question_with_file)
+    end
 
-    within '.question' do
-      expect(page).to have_link file_name(question_with_file)
-      click_on 'Delete file'
+    scenario 'can deletes attachments', js: true do
+      within '.question' do
+        expect(page).to have_link file_name(question_with_file)
+        click_on 'Delete file'
 
-      expect(page).to_not have_link file_name(question_with_file)
-      expect(page).to_not have_link 'Delete file'
+        expect(page).to_not have_link file_name(question_with_file)
+        expect(page).to_not have_link 'Delete file'
+      end
+    end
+
+    scenario 'deletes his question' do
+      expect(page).to have_content question_with_file.title
+      click_on 'Delete question'
+
+      expect(page).to have_content 'Your question deleted'
+      expect(page).to_not have_content question_with_file.title
     end
   end
 
-  scenario 'Not author can not delete attachments', js: true do
-    sign_in(not_author)
-    visit question_path(question_with_file)
-    within '.question' do
-      expect(page).to_not have_link 'Delete file'
+  describe 'Not author' do
+    before do
+      sign_in(not_author)
+      visit question_path(question_with_file)
+    end
+
+    scenario 'can not deletes attachments', js: true do
+      within '.question' do
+        expect(page).to_not have_link 'Delete file'
+      end
+    end
+
+    scenario 'can not deletes question' do
+      expect(page).to_not have_link 'Delete question'
     end
   end
 
-  scenario 'Author delete his question' do
-    sign_in(user)
-    visit question_path(question)
-    expect(page).to have_content question.title
-    click_on 'Delete question'
-
-    expect(page).to have_content 'Your question deleted'
-    expect(page).to_not have_content question.title
-  end
-
-  scenario 'Not author can not delete question' do
-    sign_in(not_author)
+  scenario 'Unauthenticated user can not deletes question' do
     visit question_path(question)
 
     expect(page).to_not have_link 'Delete question'
   end
 
-  scenario 'Unauthenticated user can not delete question' do
-    visit question_path(question)
-
-    expect(page).to_not have_link 'Delete question'
-  end
-  
   def file_name(item)
     item.files[0].filename.to_s
   end
