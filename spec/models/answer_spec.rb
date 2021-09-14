74546511cc6d906d931f3950bea09023a9d319f0
Ivan Bakhmetov <req12345@mail.ru>
2021-09-14 21:22:04 +0300

    Shared_examples unit tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it_behaves_like 'votable'
+
   it { should belong_to(:question) }
   it { should belong_to(:author) }
   it { should have_many(:links).dependent(:destroy) }
