55d37adacfd8b7ea12125f2edcaa58ae3425e7ff
Ivan Bakhmetov <req12345@mail.ru>
2021-09-24 18:33:36 +0300

    Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.html { redirect_to root_url, alert: exception.message }
+      format.js { render status: :forbidden }
+      format.json { render json: exception.message, status: :forbidden }
+    end
   end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,9 +2,7 @@ class LinksController < ApplicationController
   before_action :authenticate_user!, only: :destroy
 
   def destroy
-    if current_user
-      link.destroy if authorize! :destroy, link.linkable
-    end
+    link.destroy if authorize! :destroy, link.linkable
   end
 
   private
