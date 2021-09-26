5eb14760411112a8dfce264ebcecd2f74868b7d8
Ivan Bakhmetov <req12345@mail.ru>
2021-09-26 18:18:21 +0300

    Add serializers fo question

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -21,7 +21,7 @@ describe 'Questions API', type: :request do
       let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
       let(:question) { questions.first }
-      let(:question_response) { json.first }
+      let(:question_response) { json['questions'].first }
       let!(:answers) { create_list(:answer, 3, question: question) }
 
       before { get '/api/v1/questions', params: { access_token: access_token.token }, headers: headers }
@@ -31,28 +31,33 @@ describe 'Questions API', type: :request do
       end
 
       it 'returns list of questions' do
-        expect(json.size).to eq 2
+        expect(json['questions'].size).to eq 2
       end
 
       it 'returns all public fields' do
-        %w[id title body user_id created_at updated_at].each do |attr|
+        %w[id title body created_at updated_at].each do |attr|
           expect(question_response[attr]).to eq question.send(attr).as_json
         end
       end
 
+      it "contains user object" do
+        expect(question_response['author']['id']).to eq question.author.id
+      end
+
+      it "contains short title" do
+        expect(question_response['short_title']).to eq question.title.truncate(7)
+      end
       describe 'answers' do
-        describe 'answers' do
-          let(:answer) { answers.first }
-          let(:answer_response) { question_response['answers'].first }
+        let(:answer) { answers.first }
+        let(:answer_response) { question_response['answers'].first }
 
-          it 'returns list of answers' do
-            expect(question_response['answers'].size).to eq 3
-          end
+        it 'returns list of answers' do
+          expect(question_response['answers'].size).to eq 3
+        end
 
-          it "returns all public fields" do
-            %w[id body user_id created_at updated_at].each do |attr|
-              expect(answer_response[attr]).to eq answer.send(attr).as_json
-            end
+        it "returns all public fields" do
+          %w[id body user_id created_at updated_at].each do |attr|
+            expect(answer_response[attr]).to eq answer.send(attr).as_json
           end
         end
       end
