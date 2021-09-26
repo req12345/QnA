10aba8390bf6f66b25945f173c089b329ad5a5a1
Ivan Bakhmetov <req12345@mail.ru>
2021-09-26 22:16:20 +0300

    Fix missing current_user for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
       format.json { render json: exception.message, status: :forbidden }
     end
   end
+
+  def self.render_with_signed_in_user(user, *args)
+    ActionController::Renderer::RACK_KEY_TRANSLATION['warden'] ||= 'warden'
+    proxy = Warden::Proxy.new({}, Warden::Manager.new({})).tap{|i| i.set_user(user, scope: :user) }
+    renderer = self.renderer.new('warden' => proxy)
+    renderer.render(*args)
+  end
 end
