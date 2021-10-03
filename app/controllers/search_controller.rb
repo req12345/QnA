4396e44e313d66737f8d9ab4b975e91405500123
Ivan Bakhmetov <req12345@mail.ru>
2021-10-03 09:15:22 +0300

    Small fix Search controlller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,14 @@
 class SearchController < ApplicationController
+  skip_authorization_check
+
   def search
     service = SearchService.new(search_params)
     @result = service.call
   end
 
   private
-  
+
   def search_params
-    params.permit(:body, :type)
+    params.permit(:body, :type, :authenticity_token, :commit)
   end
 end
