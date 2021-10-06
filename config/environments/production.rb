b7aafbcb899b1a7fd84bebf667a47a1db0af445d
Ivan Bakhmetov <req12345@mail.ru>
2021-10-06 12:16:26 +0300

    Config for Mail, add credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: '64.227.67.110' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.gmail[:user_name],
+    password:             Rails.application.credentials.gmail[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
