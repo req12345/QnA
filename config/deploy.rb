e2c4523dff2687bdd15755b391614653e8ea12ee
Ivan Bakhmetov <req12345@mail.ru>
2021-10-06 17:16:34 +0300

    Config Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "qna"
+set :repo_url, "git@github.com:req12345/QnA.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key", "config/credentials/production.key", "db/seeds.rb"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
+
+set :pty, false
